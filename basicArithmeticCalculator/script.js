e6103648c2ca3c0460ff772851f3c0e0367d4e13
Blessing Ogar <104202614+everblessed31@users.noreply.github.com>
2022-06-21 00:49:11 +0100

    Add files via upload

---
+++ basicArithmeticCalculator/script.js
@@ -0,0 +1,27 @@
+
+let num1 = parseInt(prompt('Enter First Number: '))
+let operator = prompt('Enter Operator From the List (+, -, *, /): ')
+let num2 = parseInt(prompt('Enter Second Number: '))
+
+if(operator == '+')
+{
+    alert(num1 + num2)
+}
+
+
+else if(operator == '-')
+{
+    alert(num1 - num2)
+}
+
+
+else if(operator == '*')
+{
+    alert(num1 * num2)
+}
+
+
+else if(operator == '/')
+{
+    alert(num1 / num2)
+}
